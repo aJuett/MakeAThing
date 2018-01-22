418a98bb0b74aa2943706c7c2be59e259ee05283
Alissa Juett <1008677@student.sae.edu.au>
2018-01-22 10:06:17 +0800

    created repo

---
+++ Assets/Rotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    public int speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+                transform.Rotate(-Vector3.forward * speed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.RightArrow))
+                transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
