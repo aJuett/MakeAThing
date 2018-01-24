9a44a5a2c7c6e92da6c46d0c84fc0ceeab60ef1e
ALISSAS-PC\Alissa Juett <alissajuett@hotmail.com>
2018-01-24 19:11:11 +1000

    Added different kind of rortate, spawning hearts and a blip sound

---
+++ Assets/ObjSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjSpawn : MonoBehaviour {
+
+
+    public GameObject Heart;
+    public AudioSource Blip;
+
+    void Start()
+    {
+        InvokeRepeating("HeartSpawn", 1, 2); //1st one is how long itll take before it starts spawing things in, the second one is how often it will spawn
+    }
+
+    void HeartSpawn()
+    {
+        transform.position = new Vector3(-0.87f, -12.89f, 16.78f);
+        Instantiate(Heart, transform.position, Quaternion.identity);
+        Blip.Play();
+    }
+
+    
+}
+
---
+++ Assets/Rotate2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate2 : MonoBehaviour
+{
+   public int speed;
+   public float friction;
+   public float lerpSpeed;
+   public float zDeg;
+    Quaternion fromRotation;
+    Quaternion toRotation;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+            zDeg -= Input.GetAxis("Mouse X") * speed * friction;
+            fromRotation = transform.rotation;
+            toRotation = Quaternion.Euler(0, 0, zDeg);
+            transform.rotation = Quaternion.Lerp(fromRotation, toRotation, Time.deltaTime * lerpSpeed);
+        }
+    }
+}
